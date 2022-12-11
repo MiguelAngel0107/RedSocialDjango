540bb9937da5b815ab96145303f848f44b15ce08
MiguelAngel0107 <95198651+MiguelAngel0107@users.noreply.github.com>
2022-12-11 17:24:28 -0600

    update

--- core/core/settings.py
+++ core/core/settings.py
@@ -38,9 +38,16 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    
     "social",
     "tailwind",
     "theme",
+    
+    "django.contrib.sites",
+    "django.contrib.humanize",
+    'allauth',
+    'allauth.account',
+    'allauth.socialaccount',
 ]
 
 TAILWIND_APP_NAME = 'theme'
@@ -50,6 +57,15 @@ INTERNAL_IPS = [
 ]
 
 NPM_BIN_PATH = r'D:\SoftwareApp\NodeJS\npm.cmd'
+
+AUTHENTICATION_BACKENDS = [
+    # Needed to login by username in Django admin, regardless of `allauth`
+    'django.contrib.auth.backends.ModelBackend',
+    # `allauth` specific authentication methods, such as login by e-mail
+    'allauth.account.auth_backends.AuthenticationBackend',
+]
+
+SITE_ID = 1
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
