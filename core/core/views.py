3c6976e8ee79a45c6dc41f859abee6e92bb1d0d4
MiguelAngel0107 <95198651+MiguelAngel0107@users.noreply.github.com>
2022-12-14 09:38:43 -0600

    Update

--- core/core/views.py
+++ core/core/views.py
@@ -2,9 +2,9 @@
 from django.views.generic import TemplateView, View
 from django.shortcuts import redirect, render, get_object_or_404, redirect
 from django.core.paginator import Paginator
+from django.contrib.auth.mixins import UserPassesTestMixin, LoginRequiredMixin
 
-
-class HomeView(View):
+class HomeView(LoginRequiredMixin,View):
     def get(self, request, *args, **kwargs):
         
         context={
