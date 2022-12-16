ce7ba26d2619bb6d0700b58af649e34608f98838
MiguelAngel0107 <95198651+MiguelAngel0107@users.noreply.github.com>
2022-12-16 07:01:58 -0600

    Update

--- core/social/urls.py
+++ core/social/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from .views import PostEditview
+from .views import PostEditView, PostDeleteView, PostDetailView
 
 app_name='social'
 
 urlpatterns = [
-    path("edit/", PostEditview.as_view(), name="edit-post")
+    path("post/<int:pk>/", PostDetailView.as_view(), name="post-detail"),
+    path("post/edit/<int:pk>/", PostEditView.as_view(), name="post-edit"),
+    path("post/delete/<int:pk>/", PostDeleteView.as_view(), name="post-delete")
 ]
--- core/social/views.py
+++ core/social/views.py
@@ -2,19 +2,68 @@ from django.shortcuts import render
 from django.contrib.auth.mixins import UserPassesTestMixin, LoginRequiredMixin
 from django.views.generic.edit import UpdateView, DeleteView
 from django.urls.base import reverse_lazy
+from django.views.generic import View
+
+
 from .models import SocialComment, SocialPost
+
 # Create your views here.
 
+class PostDetailView(LoginRequiredMixin, View):
+    def get(self, request, pk, *args, **kwargs):
+        post = SocialPost.objects.get(pk=pk)
+        #form = SocialCommentForm()
 
-class PostEditview(LoginRequiredMixin,UserPassesTestMixin,UpdateView):
+        #comments = SocialComment.objects.filter(post=post).order_by('-created_on')
+
+        context = {
+            'post': post,
+        #    'form': form,
+        #    'comments':comments
+        }
+
+        return render(request, 'pages/social/detail.html', context)
+
+    def post(self, request, pk, *args, **kwargs):
+        post = SocialPost.objects.get(pk=pk)
+
+        #form = SocialCommentForm(request.POST)
+
+        #if form.is_valid():
+        #    new_comment = form.save(commit=False)
+        #    new_comment.author = request.user
+        #    new_comment.post = post
+        #    new_comment.save()
+
+        #comments = SocialComment.objects.filter(post=post).order_by('-created_on')
+
+        context = {
+            'post': post,
+            #'form': form,
+        #    'comments':comments
+        }
+
+        return render(request, 'pages/social/detail.html', context)
+
+
+class PostEditView(LoginRequiredMixin,UserPassesTestMixin,UpdateView):
 
     model=SocialPost
     fields=['body']
     template_name= 'pages/social/edit.html'
 
     def get_success_url(self):
-        pk= self.get_object()
+        pk= self.kwargs['pk']
         return reverse_lazy('social:post-detail', kwargs={'pk':pk})
+
+    def test_func(self):
+        post = self.get_object()
+        return self.request.user == post.author
+
+class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    model=SocialPost
+    template_name='pages/social/delete.html'
+    success_url = reverse_lazy('home')
 
     def test_func(self):
         post = self.get_object()
